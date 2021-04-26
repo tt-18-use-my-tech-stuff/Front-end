6b24858cb097df1ba57ad422adb346c9b26b2269
ALDO <aldo@aldos-mbp.attlocal.net>
2021-04-26 13:34:50 -0700

    List Items page

---
+++ src/Components/TechItem.js
@@ -0,0 +1,43 @@
+import React from "react";
+import styled from "styled-components";
+
+import {
+  Card as ReactCard,
+  CardImg,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  Button,
+  Badge,
+  CardText,
+} from "reactstrap";
+import techitems from "../img/techitems.jpg";
+
+const Card = styled(ReactCard)`
+  margin-bottom: 50px;
+`;
+
+const TechCard = ({ item }) => {
+  return (
+    <Card>
+      <CardImg top width="100%" src={techitems} alt="Recipe" />
+      <CardBody>
+        <CardTitle tag="h5">Title: {item.title}</CardTitle>
+        <CardSubtitle tag="h5" className="mb-2 text-muted">
+          Owner: {item.owner}
+        </CardSubtitle>
+        <Badge color="dark" pill>
+          Category: {item.category}
+        </Badge>
+        <CardText>Description: {item.description}</CardText>
+        <CardSubtitle tag="h5"> Daily Rental: {item.price}</CardSubtitle>
+      </CardBody>
+
+      <CardBody>
+        <Button>View item</Button>
+      </CardBody>
+    </Card>
+  );
+};
+
+export default TechCard;
---
+++ src/Components/TechList.js
@@ -0,0 +1,87 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { Container, Row, Col, Card, CardBody } from "reactstrap";
+import { SpinnerDiv, Spinner } from "../Components/styled-components/spinner";
+// import { axiosWithAuth } from "../../utils/axiosWithAuth";
+import { Button, Form, Label, Input } from "reactstrap";
+import TechItem from "./TechItem";
+import { DummyData } from "./MockData/DummyData";
+
+const TechList = () => {
+  const [allItems, setAllItems] = useState(DummyData);
+
+  const [isFetching, setIsFetching] = useState(false);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  //console.log(userId)
+
+  const handleChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+  const handleSubmit = (event) => {
+    setSearchTerm(event.target["title"].value);
+    event.preventDefault();
+  };
+
+  //   useEffect(() => {
+
+  //   }, []);
+
+  const gotallItems = allItems.length !== 0 ? true : false;
+
+  if (isFetching)
+    return (
+      <SpinnerDiv>
+        <Spinner color="success" />
+      </SpinnerDiv>
+    );
+  else if (gotallItems)
+    return (
+      <Container>
+        <div>
+          <Form onSubmit={handleSubmit}>
+            <div className="searchBox">
+              <Input
+                id="title"
+                name="title"
+                type="text"
+                placeholder="Search items"
+                onChange={handleChange}
+                value={searchTerm}
+              />
+              <Button type="submit">Search</Button>
+            </div>
+          </Form>
+        </div>
+        <Row>
+          {allItems.map((item) => {
+            if (
+              item.title &&
+              item.title.toLowerCase().includes(searchTerm.toLowerCase())
+            ) {
+              return (
+                <Col xs="12" sm="6" md="4" key={item.id}>
+                  <TechItem item={item} />
+                </Col>
+              );
+            } else return null;
+          })}
+        </Row>
+      </Container>
+    );
+  else if (!gotallItems)
+    return (
+      <Container style={{ margin: "50px auto" }}>
+        <Row>
+          <Col xs="12" lg={{ size: 4, offset: 4 }}>
+            <Card>
+              <CardBody>No recipes yet?</CardBody>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    );
+  else return <></>;
+};
+
+export default TechList;
