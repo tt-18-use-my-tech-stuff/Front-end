0c5edc4ef471aba6c172e385d4b19fc5076af1ca
Taylor Kang-Illescas <taylorkangillescas@Taylors-MacBook-Pro-8.local>
2021-04-27 16:35:12 -0700

    useLogin, itemEdit

---
+++ src/Components/itemEdit.js
@@ -0,0 +1,99 @@
+import React, { useEffect, useState } from 'react';
+// import { axiosWithAuth } from '../helpers/axiosWithAuth'; 
+import { useHistory, useParams } from 'react-router-dom'
+
+const itemEdit = () => {
+    const { id } = useParams();
+    const history = useHistory();
+    const [ item, setItem ] = useState([]);
+    const [ itemName, setItemName ] = useState({});
+    const [ itemDescription, setItemDescription ] = useState({});
+
+    useEffect(() => {
+        axiosWithAuth()
+        .get() // add
+        .then((res) => {
+            console.log("item edit response", res.data);
+            setItem(res.data);
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+    }, []);
+
+    const editName = (ev) => { //combine into object
+        setItemName({ ...itemName, [ev.target.name] : ev.target.value });
+    };
+
+    const editDescription = (ev) => {
+        setItemDescription({ ...itemDescription, [ev.target.name] : ev.target.value });
+    };
+
+    const cancel = (e) => {
+        e.preventDefault();
+        history.push(``) // add
+    };
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+        console.log("put item in submit handler");
+        
+        axiosWithAuth()
+        .put() // add
+        .then(() => {
+            console.log("name was changed")
+        })
+        .catch((err) => {
+            console.log(err.response)
+        })
+
+        axiosWithAuth()
+        .put() // add
+        .then(() => {
+            console.log("description was changed")
+        })
+        .catch((err) => {
+            console.log(err.response)
+        })
+
+        history.push() // add
+    }
+
+    return (
+        <Container>
+      <Row>
+        <Col xs="12" md={{ size: 6, offset: 3 }}>
+          <FormContainer>
+            <h3>Edit an Item</h3>
+            <form onSubmit={submitHandler}>
+              <label>
+                Item Name:
+                <Input name="name" value={item.name} onChange={editName} />
+              </label>
+
+              <label>
+                Description:
+                <Input
+                  name="description"
+                  value={item.description}
+                  onChange={editDescription}
+                />
+              </label>
+              <div>
+                <Button
+                  type="submit"
+                  disabled={!item.name || !item.description}
+                >
+                  Save Changes
+                </Button>
+              </div>
+              {itemValue()}
+            </form>
+          </FormContainer>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+
