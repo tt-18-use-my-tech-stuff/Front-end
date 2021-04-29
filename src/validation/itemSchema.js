c5a0f7be21db1f232287d5f1771c67f00705c39e
abe-one <77995299+abe-one@users.noreply.github.com>
2021-04-29 12:08:08 -0500

    item schema

--- src/validation/itemSchema.js
+++ src/validation/itemSchema.js
@@ -3,6 +3,10 @@ import * as yup from "yup";
 export default yup.object().shape({
   item_name: yup.string().required("Item name is required"),
   item_description: yup.string().required("Description is required"),
-  price: yup.number().required("Price is required"),
-  category: yup.string().required("Category required")
+  price: yup
+    .number()
+    .min(1, `Price must be at least $1`)
+    .max(999999.99, `Price must be at most $999999.99`)
+    .required(`Price is required`),
+  category: yup.string().required("Category required"),
 });
